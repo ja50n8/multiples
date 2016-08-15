738f0f0917d9498eda37c4f0fa955e48df2a8869
Jason Bell <ja50n8@yahoo.co.uk>
2016-08-15 13:12:25 +0100

    Multiples iOS Course

--- multiples-of-two/ViewController.swift
+++ multiples-of-two/ViewController.swift
@@ -10,16 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    //IBOutlets
+    @IBOutlet weak var whatMultipleTxt: UITextField!
+    @IBOutlet weak var sumLbl: UILabel!
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    //Properties
+    var multiple = 0
+    var runningSum = 0
+    let MAX_SUM = 50
+ 
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onAddBtnTapped(sender: AnyObject) {
+        //Since we never change the newSum value make it a constant
+        let newSum = runningSum + multiple
+        updateLbl(runningSum, mul: multiple, newSum: newSum)
+        runningSum += multiple
+        
+        if isGameOver() {
+            restartGame()
+        }
     }
-
+    
+    @IBAction func onPlayBtnTapped(sender: AnyObject) {
+        
+        if whatMultipleTxt.text != nil && whatMultipleTxt.text != "" {
+            
+            whatMultipleTxt.hidden = true
+            logoImg.hidden = true
+            playBtn.hidden = true
+            sumLbl.hidden = false
+            addBtn.hidden = false
+            
+            resetLbl()
+            multiple = Int(whatMultipleTxt.text!)!
+            runningSum = 0
+        }
+    }
+    
+    func restartGame() {
+        multiple = 0
+        whatMultipleTxt.text = ""
+        whatMultipleTxt.hidden = false
+        logoImg.hidden = false
+        playBtn.hidden = false
+        sumLbl.hidden = true
+        addBtn.hidden = true
+        resetLbl()
+    }
+    
+    func resetLbl() {
+        sumLbl.text = "Press Add to add!"
+    }
+    
+    func isGameOver() -> Bool {
+        if runningSum >= MAX_SUM {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func updateLbl(oldSum: Int, mul: Int, newSum: Int) {
+        sumLbl.text = "\(oldSum) + \(mul) = \(newSum)"
+    }
+    
 
 }
 
